725bb91db3b863f2b4ad06ed23151c5ed19555ab
aazaaa <benito.ernst@t-online.de>
2020-10-10 18:31:47 +0200

    uppp

---
+++ Database/Book.cs
@@ -0,0 +1,17 @@
+namespace GrapQl_API.Database
+{
+  public class Book
+  {
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    public bool Published { get; set; }
+
+    public string Genre { get; set; }
+
+    public string AuthorId { get; set; }
+
+    public Author Author { get; set; }
+  }
+}
